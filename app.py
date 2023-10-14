869e865a04a37e3dfae936a03fcdaaba9e52e968
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-10-14 20:38:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Sample JSON data with input types
+initial_data = {
+    "fields": [
+        {"label": "Field 1", "type": "text", "value": "Value 1"},
+        {"label": "Field 2", "type": "dropdown", "options": ["Option 1", "Option 2"], "value": "Option 1"},
+        {"label": "Field 3", "type": "radio", "options": ["Option A", "Option B"], "value": "Option A"},
+        {"label": "Field 4", "type": "calendar", "value": "2023-01-01"},
+        {"label": "Field 5", "type": "number", "value": 42}
+    ]
+}
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    return jsonify(initial_data)
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    if request.is_json:
+        try:
+            new_data = request.get_json()
+            # Handle the new data as needed (e.g., update a database)
+            print("Received data:", new_data)
+            return jsonify({"status": "success"})
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)})
+    else:
+        return jsonify({"status": "error", "message": "Invalid JSON data"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
