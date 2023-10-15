6e6389a884ccdf9b950afa125032542bb7a9870c
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-10-15 14:04:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-# Sample JSON data with input types
-initial_data = {
-    "fields": [
-        {"label": "Field 1", "type": "text", "value": "Value 1"},
-        {"label": "Field 2", "type": "dropdown", "options": ["Option 1", "Option 2"], "value": "Option 1"},
-        {"label": "Field 3", "type": "radio", "options": ["Option A", "Option B"], "value": "Option A"},
-        {"label": "Field 4", "type": "calendar", "value": "2023-01-01"},
-        {"label": "Field 5", "type": "number", "value": 42}
-    ]
-}
+# File path to store JSON data
+data_file_path = 'data.json'
+
+def read_data():
+    try:
+        with open(data_file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        # If the file is not found, return an empty initial data
+        return {"fields": []}
+
+def write_data(data):
+    with open(data_file_path, 'w') as file:
+        json.dump(data, file, indent=2)
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
-    return jsonify(initial_data)
+    data = read_data()
+    return jsonify(data)
 
 @app.route('/save_data', methods=['POST'])
 def save_data():
@@ -24,6 +31,10 @@ def save_data():
             new_data = request.get_json()
             # Handle the new data as needed (e.g., update a database)
             print("Received data:", new_data)
+            
+            # Update the data in the file
+            write_data(new_data)
+            
             return jsonify({"status": "success"})
         except Exception as e:
             return jsonify({"status": "error", "message": str(e)})
