0df5c91a6c241b1d9c640bce58dfbcb12073d104
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-10-15 15:01:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,41 @@ from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
+@app.route('/edit_data', methods=['GET', 'POST'])
+def edit_data():
+    if request.method == 'POST':
+        try:
+            # Read the updated data from the HTML form
+            updated_data = {
+                "fields": []
+            }
+            for key, value in request.form.items():
+                field_info = key.split('_')
+                index = int(field_info[1])
+                field_type = field_info[0]
 
+                if index >= len(updated_data["fields"]):
+                    updated_data["fields"].append({})
+
+                if field_type == 'label':
+                    updated_data["fields"][index]["label"] = value
+                elif field_type == 'type':
+                    updated_data["fields"][index]["type"] = value
+                elif field_type == 'options':
+                    updated_data["fields"][index]["options"] = value.split(',')
+                elif field_type == 'value':
+                    updated_data["fields"][index]["value"] = value
+
+            # Update the data in the file
+            write_data(updated_data)
+
+            return jsonify({"status": "success"})
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)})
+    else:
+        # Read the current data to display in the HTML form
+        current_data = read_data()
+        return render_template('edit_data.html', current_data=current_data)
 # File path to store JSON data
 data_file_path = 'data.json'
 
