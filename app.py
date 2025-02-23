cdfad602887789208b45992f2d2ebde4fccccf21
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2025-02-24 00:50:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,88 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 app = Flask(__name__)
-@app.route('/dash')
-def show_user():
+TEMPLATE = u"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PivotTable.js</title>
+
+        <!-- external libs from cdnjs -->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
+
+
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/pivot.min.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/pivot.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/d3_renderers.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/c3_renderers.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/export_renderers.min.js"></script>
+
+        <style>
+            body {font-family: Verdana;}
+            .node {
+              border: solid 1px white;
+              font: 10px sans-serif;
+              line-height: 12px;
+              overflow: hidden;
+              position: absolute;
+              text-indent: 2px;
+            }
+            .c3-line, .c3-focused {stroke-width: 3px !important;}
+            .c3-bar {stroke: white !important; stroke-width: 1;}
+            .c3 text { font-size: 12px; color: grey;}
+            .tick line {stroke: white;}
+            .c3-axis path {stroke: grey;}
+            .c3-circle { opacity: 1 !important; }
+            .c3-xgrid-focus {visibility: hidden !important;}
+        </style>
+    </head>
+    <body>
+        <script type="text/javascript">
+            $(function(){
+                if(window.location != window.parent.location)
+                    $("<a>", {target:"_blank", href:""})
+                        .text("[pop out]").prependTo($("body"));
+
+                $("#output").pivotUI(
+                    $.csv.toArrays($("#output").text()),
+                    $.extend({
+                        renderers: $.extend(
+                            $.pivotUtilities.renderers,
+                            $.pivotUtilities.c3_renderers,
+                            $.pivotUtilities.d3_renderers,
+                            $.pivotUtilities.export_renderers
+                            ),
+                        hiddenAttributes: [""]
+                    }, %(kwargs)s)
+                ).show();
+             });
+        </script>
+        <div id="output" style="display: none;">%(csv)s</div>
+    </body>
+</html>
+"""
+import json, io
+def pivot_ui(df, outfile_path = "pivottablejs.html", url="",
+    width="100%", height="500", **kwargs):
+    with io.open(outfile_path, 'wt', encoding='utf8') as outfile:
+        csv = df.to_csv(encoding='utf8')
+
+    return TEMPLATE %
+            dict(csv=csv, kwargs=json.dumps(kwargs))
+def show_user(url):
  #return  request.args.get('url', 'No URL provided')
  datas=[]
  # URL of the webpage to scrape
  p=1
  stop= False
  while not stop:
-  url = request.args.get('url', 'No URL provided')#request.path.split("/dash/")[-1]#"https://www.mubawab.ma/fr/sd/tanger/malabata/appartements-a-vendre"+":p:"+str(p)
+  url = request.args.get('url', 'No URL provided')+":p:"+str(p)#request.path.split("/dash/")[-1]#"https://www.mubawab.ma/fr/sd/tanger/malabata/appartements-a-vendre"
 
 # Send a GET request to fetch the page content
   response = requests.get(url)
@@ -51,7 +124,13 @@ def show_user():
 
 # Convert to DataFrame
  df = pd.DataFrame(datas)
- return df.to_html(index=False)
+ return df
+@app.route('/table')
+def tester():
+ return show_user(url).to_html(index=False)
+@app.route('/dash')
+def tester():
+ return pivot_ui(show_user(url))
 # Export to Excel
 # df.to_excel("output3.xlsx", index=False)
 
